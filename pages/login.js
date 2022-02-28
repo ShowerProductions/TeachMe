16ebda3887dce372968275052dfa3ecc0da0b235
ShowerProductions <showerproductionsbuisness@gmail.com>
2022-02-27 21:23:49 -0800

    Added login prompt

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,17 @@
 import Head from 'next/head';
 import Layout from '@components/layout/Layout';
 
+import InputForm from '@components/authentication/input-form/InputForm';
+import InputItem from '@components/authentication/input-item/InputItem';
+
 export default function Login() {
   return (
-    <Layout>
+    <div>
       <Head></Head>
-    </Layout>
+      <InputForm name="Login">
+        <InputItem name="Username" />
+        <InputItem name="Password" />
+      </InputForm>
+    </div>
   );
 }
